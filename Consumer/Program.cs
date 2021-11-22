6c6255311ca556ee09a4b1b6fdef91187ee4ffd8
Ho Thanh Hung <hunght18@fpt.com.vn>
2021-11-22 16:38:56 +0700

    Default Queue

---
+++ Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+class Consumer
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+        using var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+        //channel.QueueDeclare("hello-queue", true, false, false);
+
+        // string message = "hello world";
+        // var messageBody = Encoding.UTF8.GetBytes(message);
+        // channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+        channel.BasicQos(0, 5, false);
+        var consumer = new EventingBasicConsumer(channel);
+        channel.BasicConsume("hello-queue", false, consumer);
+        consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            Console.WriteLine($"Message: {message}");
+            channel.BasicAck(e.DeliveryTag, false);
+        };
+
+        Console.ReadLine();
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+        using var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+        channel.QueueDeclare("hello-queue", true, false, false);
+
+        string message = "hello world 10";
+        for (int i = 100; i < 200; i++)
+        {
+            message = i.ToString();
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+            Thread.Sleep(100);
+        }
+
+
+        Console.WriteLine("Message: Done");
+        Console.ReadLine();
+    }
+}
