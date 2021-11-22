7e2786165ac0e5f1cca42450b27f550adab5d997
Ho Thanh Hung <hunght18@fpt.com.vn>
2021-11-22 17:19:49 +0700

    Exchange: Fanout -- All queue (randomQueue)

---
+++ 1.FanoutExchange/Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+//Exchange: Fanout
+string exchange = "logs-fanout";
+channel.ExchangeDeclare(exchange, durable: true, type: ExchangeType.Fanout);
+
+string message = "hello world 10";
+for (int i = 100; i < 200; i++)
+{
+    message = i.ToString() + exchange;
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange, "", null, messageBody);
+    Thread.Sleep(100);
+}
+
+Console.WriteLine("Message: Done");
+Console.ReadLine();
